17ab8793c16a1925b826d991b19fc63e10d93423
ProgramadorGod <pipenet12@gmail.com>
2023-03-23 00:23:56 -0500

    moreless working

--- GOD/src/Yisus/HospitalSystem.java
+++ GOD/src/Yisus/HospitalSystem.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 
 public class HospitalSystem {
 
+    private static Object option;
+    static boolean Correct = false;
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         ArrayList<ArrayList> UsersList = new ArrayList<>();
@@ -23,15 +25,42 @@ public class HospitalSystem {
             System.out.println("2 . Generate date.");
             System.out.println("3 . Get users");
             System.out.println("4 . Exit.");
-            
-            int option = in.nextInt();
+
+
+
+
+            int option = 0;
+            while (option == 0) {
+                try {
+                    option = in.nextInt();
+                    if(option == 0){
+                        System.out.println("Try again ... ");
+                    }
+
+                } catch (Exception e) {
+                    System.out.println("Try again...");
+                    in.nextLine();
+                }
+            }
+
+
             switch (option){
                 case 1:
-                    int Type = 1;
+                    int Type = 0;
 
                     do {
                         System.out.print("User Type 1(Patient) / 2(Doctor): ");
-                        Type = in.nextInt();
+                        while (Type == 0)
+                        try {
+                            Type = in.nextInt();
+                            if(Type == 0){
+                                System.out.println("Error inserting the type, please choose a valid option(1/2)");
+                            }
+                        }catch (Exception e){
+                            System.out.println("Error inserting the type, please choose a valid option(1/2)");
+                            in.nextLine();
+                        }
+
                         if(Type != 1 && Type != 2){
                             System.out.println("This option it's not available. Try Again...");
                         }
@@ -51,6 +80,84 @@ public class HospitalSystem {
 
                 case 2:
                     System.out.println("This are the patients waiting for a date : ");
+                    int PatientNum = 1;
+                    int Selected = 0;
+                    for (int i = 0; i < PatientsList.size() ; i++) {
+
+                        fat = PatientsList.get(i).size();
+                        if(fat != 0) {
+                            for (int j = 0; j < PatientsList.get(i).size(); j++) {
+                                System.out.println("Patient #"+String.valueOf(PatientNum) + " : " + PatientsList.get(i).get(j));
+                                PatientNum += 1;
+                            }
+                        }else{
+                            System.out.println("Any patient to make date.");
+                            break;
+                        }
+                    };
+
+                    if(PatientNum>1){
+                        System.out.print("Please insert the number of the patient you're gonna program a date: ");
+                        while (Selected == 0 || Selected < 0 || Selected >PatientNum){
+                            try{
+                                Selected = in.nextInt();
+                                if(Selected <= 0 || Selected >PatientNum){
+                                    System.out.println("Try again with a valid patient");
+                                }
+
+                            }catch (Exception e){
+                                System.out.println("Error. Try Again...");
+                                in.nextLine();
+                            }
+                        }
+                        System.out.println("\nPatient #"+Selected + " chosen: " + PatientsList.get(Selected-1).get(0));
+
+                    }else {
+                        System.out.println("There's no patients to date");
+                    }
+
+
+
+                    System.out.println("Now choose the doctor.");
+                    int DoctorNum = 1;
+                    Selected = 0;
+                    for (int i = 0; i < DoctorsList.size() ; i++) {
+
+                        fat = DoctorsList.get(i).size();
+                        if(fat != 0) {
+                            for (int j = 0; j < DoctorsList.get(i).size(); j++) {
+                                System.out.println("Doctor #"+String.valueOf(DoctorNum) + " : " + DoctorsList.get(i).get(j));
+                                DoctorNum += 1;
+                                System.out.println("i : "+ i + " j: " + j );
+                            }
+                        }else{
+                            System.out.println("Any patient to make date.");
+                        }
+                    };
+                    if(DoctorNum>1){
+                        System.out.print("Please insert the number of the doctor you're gonna program a date: ");
+                        while (Selected == 0 || Selected < 0 || Selected >DoctorNum){
+                            try{
+                                Selected = in.nextInt();
+                                if(Selected == 0 || Selected < 0 || Selected > DoctorNum){
+                                    System.out.println("Try again with a valid doctor");
+                                }
+
+                            }catch (Exception e){
+                                System.out.println("Error. Try Again...");
+                                in.nextLine();
+                            }
+                        }
+                        System.out.println("\nDoctor #"+Selected + " chosen: " + DoctorsList.get(Selected-1).get(0));
+                    }else{
+                        System.out.println("There's no doctors to make the date");
+                    }
+
+
+
+
+
+
 
 
                     break;
@@ -69,7 +176,6 @@ public class HospitalSystem {
                             }else{
                                 System.out.println("Patients list it's empty");
                             }
-
 
                         };
                         System.out.println();
